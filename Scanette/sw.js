10fa1710def5fa90cf2d36f324998eca73fd0927
ybenakmo <yacine.benakmoume@edu.univ-fcomte.fr>
2019-11-19 11:54:30 +0100

    Scanette

--- Scanette/sw.js
+++ Scanette/sw.js
@@ -1,3 +1,30 @@
+var cacheName = 'sw';
+var contentToCache = [
+  '/Scanette/',
+  '/Scanette/index.html',
+  '/Scanette/style.css',
+  '/Scanette/manifest.json',
+  '/Scanette/sw.js',
+  '/Scanette/produits.csv',
+  '/Scanette/js/DecoderWorker.js',
+  '/Scanette/js/exif.js',
+  '/Scanette/js/job.js',
+  '/Scanette/js/app.js',
+  '/Scanette/favicon.ico',
+  '/Scanette/images/logo.png',
+  '/Scanette/images/barcode-scanner.png',
+  '/Scanette/images/icon-setup.png',
+  '/Scanette/images/icon-transmit.png',
+  '/Scanette/icons/icon-32.png',
+  '/Scanette/icons/icon-64.png',
+  '/Scanette/icons/icon-96.png',
+  '/Scanette/icons/icon-128.png',
+  '/Scanette/icons/icon-168.png',
+  '/Scanette/icons/icon-192.png',
+  '/Scanette/icons/icon-256.png',
+  '/Scanette/icons/icon-512.png'
+];
+
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
   e.waitUntil(
@@ -23,29 +50,3 @@ self.addEventListener('fetch', (e) => {
   );
 });
 
-var cacheName = 'sw';
-var appShellFiles = [
-  '/Scanette/',
-  '/Scanette/index.html',
-  '/Scanette/style.css',
-  '/Scanette/manifest.json',
-  '/Scanette/sw.js',
-  '/Scanette/produits.csv',
-  '/Scanette/js/DecoderWorker.js',
-  '/Scanette/js/exif.js',
-  '/Scanette/js/job.js',
-  '/Scanette/js/app.js',
-  '/Scanette/favicon.ico',
-  '/Scanette/images/logo.png',
-  '/Scanette/images/barcode-scanner.png',
-  '/Scanette/images/icon-setup.png',
-  '/Scanette/images/icon-transmit.png',
-  '/Scanette/icons/icon-32.png',
-  '/Scanette/icons/icon-64.png',
-  '/Scanette/icons/icon-96.png',
-  '/Scanette/icons/icon-128.png',
-  '/Scanette/icons/icon-168.png',
-  '/Scanette/icons/icon-192.png',
-  '/Scanette/icons/icon-256.png',
-  '/Scanette/icons/icon-512.png'
-];
